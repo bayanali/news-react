d52f40f3e6c20431aede1869349201befa2acd4c
Salma Hassan <salma.hassanothman@gmail.com>
2018-05-04 13:49:20 +0200

    adding carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import './CSS/header.css'
 import Footer from './components/footer/footer';
-import NavBar from './components/Header/navBar';
+import Header from './components/Header/header';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
+        <Header/>
+        
         <Footer/>
       </div>
 
--- src/components/Header/navSearch.js
+++ src/components/Header/navSearch.js
@@ -3,7 +3,7 @@ import React from 'react'
 const navSearch = () => {
   return (
     <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2 transparent-search" type="text" placeholder= "&#xF002 Search" />
+      <input className="form-control mr-sm-2 transparent-search" type="text" placeholder= "Search" />
     </form>
   );
 };
